0eb769d3c062d223194181ea74f3ebf63bc7fea0
Zack Huston <zackhuston@Zacks-MacBook-Air.local>
2022-09-18 12:09:54 -0400

    Added Prompts and Confirms

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 // Assignment code here
 
+var confirmLength;
+var confirmLowercase;
+var confirmUppercase;
+var confirmNumeric;
+var confirmSpecialChar;
+
+function generatePassword(){
+   confirmLength = prompt(("How many characters would you like it to contain?"))
+   confirmLowercase = confirm("Would you like to include Lowercase characters?")
+   confirmUppercase = confirm("Would you like to include Uppercase characters?")
+   confirmNumeric = confirm ("Would you like to include Numbers?")
+   confirmSpecialChar = confirm("Would you like to include Special characters?")
+  
+  if (confirmLength <=7 || confirmLength >= 129 ){
+    alert("Length of password may not be longer than 129 characters and shorter than 7.")
+  }
+
+  if (confirmLowercase === false || confirmUppercase === false || confirmNumeric === false
+    || confirmSpecialChar === false){
+      passwordText.value = password;
+    }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -9,9 +31,13 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+
   passwordText.value = password;
 
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
